d5ee8242b66ed5c2c41a582f3719c7e364380ded
Sriram Murali <Sriram.Murali@hsrw.org>
2019-04-15 18:58:22 +0200

    Commit for Testing Repo
    GOT

--- HolidayAssistant/Controllers/ValuesController.cs
+++ HolidayAssistant/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace HolidayAssistant.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Jon Snow", "King in the North" };
         }
 
         // GET api/values/5
